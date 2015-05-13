829e8b18aa36e82013378f57c96500275d6e585c
8rrico <8rrico@gmail.com>
2015-05-13 14:05:52 -0400

    Respace

--- src/com/gmail/fedmanddev/VillagerTrade.java
+++ src/com/gmail/fedmanddev/VillagerTrade.java
@@ -4,49 +4,51 @@ import org.bukkit.inventory.*;
 
 public final class VillagerTrade
 {
-    private ItemStack primaryItem;
-    private ItemStack secondaryItem;
-    private ItemStack rewardItem;
-    
-    public VillagerTrade(final ItemStack primaryItem, final ItemStack secondaryItem, final ItemStack rewardItem) {
-        this.primaryItem = secondaryItem;
-        this.secondaryItem = secondaryItem;
-        this.rewardItem = rewardItem;
-    }
-    
-    public VillagerTrade(final ItemStack primarySlot, final ItemStack rewardItem) {
-        this.secondaryItem = primarySlot;
-        this.rewardItem = rewardItem;
-    }
-    
-    @Deprecated
-    public static boolean hasItem2(final VillagerTrade villagerTrade) {
-        return villagerTrade.secondaryItem != null;
-    }
-    
-    public static boolean hasSecondaryItem(final VillagerTrade villagerTrade) {
-        return villagerTrade.secondaryItem != null;
-    }
-    
-    @Deprecated
-    public static ItemStack getItem1(final VillagerTrade villagerTrade) {
-        return villagerTrade.secondaryItem;
-    }
-    
-    public static ItemStack getPrimaryItem(final VillagerTrade villagerTrade) {
-        return villagerTrade.primaryItem;
-    }
-    
-    @Deprecated
-    public static ItemStack getItem2(final VillagerTrade villagerTrade) {
-        return villagerTrade.secondaryItem;
-    }
-    
-    public static ItemStack getSecondaryItem(final VillagerTrade villagerTrade) {
-        return villagerTrade.secondaryItem;
-    }
-    
-    public static ItemStack getRewardItem(final VillagerTrade villagerTrade) {
-        return villagerTrade.rewardItem;
-    }
+
+	private ItemStack primaryItem;
+	private ItemStack secondaryItem;
+	private ItemStack rewardItem;
+
+	public VillagerTrade(final ItemStack primaryItem, final ItemStack secondaryItem, final ItemStack rewardItem) {
+		this.primaryItem = secondaryItem;
+		this.secondaryItem = secondaryItem;
+		this.rewardItem = rewardItem;
+	}
+
+	public VillagerTrade(final ItemStack primarySlot, final ItemStack rewardItem) {
+		this.secondaryItem = primarySlot;
+		this.rewardItem = rewardItem;
+	}
+
+	@Deprecated
+	public static boolean hasItem2(final VillagerTrade villagerTrade) {
+		return villagerTrade.secondaryItem != null;
+	}
+
+	public static boolean hasSecondaryItem(final VillagerTrade villagerTrade) {
+		return villagerTrade.secondaryItem != null;
+	}
+
+	@Deprecated
+	public static ItemStack getItem1(final VillagerTrade villagerTrade) {
+		return villagerTrade.secondaryItem;
+	}
+
+	public static ItemStack getPrimaryItem(final VillagerTrade villagerTrade) {
+		return villagerTrade.primaryItem;
+	}
+
+	@Deprecated
+	public static ItemStack getItem2(final VillagerTrade villagerTrade) {
+		return villagerTrade.secondaryItem;
+	}
+
+	public static ItemStack getSecondaryItem(final VillagerTrade villagerTrade) {
+		return villagerTrade.secondaryItem;
+	}
+
+	public static ItemStack getRewardItem(final VillagerTrade villagerTrade) {
+		return villagerTrade.rewardItem;
+	}
+
 }
--- src/com/gmail/fedmanddev/VillagerTradeApi.java
+++ src/com/gmail/fedmanddev/VillagerTradeApi.java
@@ -10,41 +10,41 @@ import net.minecraft.server.v1_7_R4.*;
 public final class VillagerTradeApi extends JavaPlugin
 {
 
-    public static void clearTrades(final Villager villager) {
-        final EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
-        try {
-            final Field recipes = entityVillager.getClass().getDeclaredField("bu");
-            recipes.setAccessible(true);
-            final MerchantRecipeList list = new MerchantRecipeList();
-            recipes.set(entityVillager, list);
-        }
-        catch (Exception exc) {
-            exc.printStackTrace();
-        }
-    }
-    
-    public static void addTrade(final Villager villager, final VillagerTrade villagerTrade) {
-        final EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
-        try {
-            final Field recipes = entityVillager.getClass().getDeclaredField("bu");
-            recipes.setAccessible(true);
-            final MerchantRecipeList list = (MerchantRecipeList)recipes.get(entityVillager);
-            if (VillagerTrade.hasSecondaryItem(villagerTrade)) {
-                final ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getPrimaryItem(villagerTrade));
-                final ItemStack item2 = CraftItemStack.asNMSCopy(VillagerTrade.getSecondaryItem(villagerTrade));
-                final ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
-                list.a(new MerchantRecipe(item1, item2, rewardItem));
-            }
-            else {
-                final ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getPrimaryItem(villagerTrade));
-                final ItemStack rewardItem2 = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
-                list.a(new MerchantRecipe(item1, rewardItem2));
-            }
-            recipes.set(entityVillager, list);
-        }
-        catch (Exception exc) {
-            exc.printStackTrace();
-        }
-    }
+	public static void clearTrades(final Villager villager) {
+		final EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
+		try {
+			final Field recipes = entityVillager.getClass().getDeclaredField("bu");
+			recipes.setAccessible(true);
+			final MerchantRecipeList list = new MerchantRecipeList();
+			recipes.set(entityVillager, list);
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	public static void addTrade(final Villager villager, final VillagerTrade villagerTrade) {
+		final EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
+		try {
+			final Field recipes = entityVillager.getClass().getDeclaredField("bu");
+			recipes.setAccessible(true);
+			final MerchantRecipeList list = (MerchantRecipeList)recipes.get(entityVillager);
+			if (VillagerTrade.hasSecondaryItem(villagerTrade)) {
+				final ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getPrimaryItem(villagerTrade));
+				final ItemStack item2 = CraftItemStack.asNMSCopy(VillagerTrade.getSecondaryItem(villagerTrade));
+				final ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
+				list.a(new MerchantRecipe(item1, item2, rewardItem));
+			}
+			else {
+				final ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getPrimaryItem(villagerTrade));
+				final ItemStack rewardItem2 = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
+				list.a(new MerchantRecipe(item1, rewardItem2));
+			}
+			recipes.set(entityVillager, list);
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
 
 }
