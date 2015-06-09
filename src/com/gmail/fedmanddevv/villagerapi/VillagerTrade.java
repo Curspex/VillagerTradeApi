32b00efb3f274193a28cc027cb406d9f792f9ee1
8rrico <8rrico@gmail.com>
2015-06-09 12:33:04 -0400

    [BREAKING] Refactor package declaration

--- src/com/gmail/fedmanddevv/villagerapi/VillagerTrade.java
+++ src/com/gmail/fedmanddevv/villagerapi/VillagerTrade.java
@@ -1,4 +1,4 @@
-package com.gmail.fedmanddev;
+package com.gmail.fedmanddevv.villagerapi;
 
 import org.bukkit.inventory.ItemStack;
 
--- src/com/gmail/fedmanddevv/villagerapi/VillagerTradeApi.java
+++ src/com/gmail/fedmanddevv/villagerapi/VillagerTradeApi.java
@@ -1,4 +1,4 @@
-package com.gmail.fedmanddev;
+package com.gmail.fedmanddevv.villagerapi;
 
 import java.lang.reflect.Field;
 
